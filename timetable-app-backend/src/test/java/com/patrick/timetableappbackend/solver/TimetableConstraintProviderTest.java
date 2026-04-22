71cd04475e06ffd31a1139ca1de02a72c2ab5ae2
Diallo Francis-Patrick <diallofrancispatrick@gmail.com>
2026-04-22 15:37:59 +0300

    Add TestPropertySource import to TimetableConstraintProviderTest

--- timetable-app-backend/src/test/java/com/patrick/timetableappbackend/solver/TimetableConstraintProviderTest.java
+++ timetable-app-backend/src/test/java/com/patrick/timetableappbackend/solver/TimetableConstraintProviderTest.java
@@ -13,6 +13,7 @@ import com.patrick.timetableappbackend.model.Year;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
