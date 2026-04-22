efe9184b5c4a42730226dd11411f45b1f2a1f62a
PatrickDiallo23 <diallofrancispatrick@gmail.com>
2026-04-22 22:20:37 +0300

    Fix Bean issue

--- timetable-app-backend/src/test/java/com/patrick/timetableappbackend/solver/TimetableConstraintProviderTest.java
+++ timetable-app-backend/src/test/java/com/patrick/timetableappbackend/solver/TimetableConstraintProviderTest.java
@@ -14,18 +14,19 @@ import com.patrick.timetableappbackend.model.Timetable;
 import com.patrick.timetableappbackend.model.Year;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.Set;
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.NONE,
-    classes = ConstraintConfig.class
-)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ConstraintConfig.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class TimetableConstraintProviderTest {
 
